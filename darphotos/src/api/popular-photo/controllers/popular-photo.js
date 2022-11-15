88c8a78bdae464de0482b823784231206cfc8be3
Darryl Becker <dar.becker12@pm.me>
2022-11-15 15:23:06 -0700

    Added all

---
+++ darphotos/src/api/popular-photo/controllers/popular-photo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * popular-photo controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::popular-photo.popular-photo');
---
+++ darphotos/src/api/popular-photo/routes/popular-photo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * popular-photo router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::popular-photo.popular-photo');
